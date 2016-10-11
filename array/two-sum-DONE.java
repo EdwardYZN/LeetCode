99df624c79b811730c74f410adb581fbd399f86e
yange <yange@apple.com>
2016-10-11 11:42:01 +0800

    Array:two-sums

--- array/two-sum-DONE.java
+++ array/two-sum-DONE.java
@@ -13,6 +13,19 @@ array is sorted  (E) Two Sum III - Data structure design
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        HashMap<Integer,Integer> map=new HashMap();
+        int[] result=new int[2];
+
+        for(int i=0; i<nums.length; i++){
+          if(map.containsKey(target-nums[i])){
+            result[0]=map.get(target-nums[i]);
+            result[1]=i;
+            return result;
+          }
+          else{
+            map.put(nums[i],i);
+          }
+        }
+        return result;
     }
 }
