408fa49af48b90cf030b84ae94f33a566e26a261
EdwardYZN <YangZhennanEdward@gmail.com>
2016-10-15 22:44:17 +0800

    Array:contains-duplicate

--- array/contains-duplicate-DONE.java
+++ array/contains-duplicate-DONE.java
@@ -9,6 +9,14 @@ Similar Problems   (E) Contains Duplicate II  (M) Contains Duplicate III
 
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
+        HashSet<Integer> set=new HashSet<Integer>();
+
+        for(int i=0;i<nums.length;i++){
+          if(set.contains(nums[i]))
+            return true;
+          else
+            set.add(nums[i]);
+        }
+        return false;
     }
 }
--- array/contains-duplicate-ii-DONE.java
+++ array/contains-duplicate-ii-DONE.java
@@ -9,6 +9,15 @@ asked this question    Show Tags   Array  Hash Table     Show Similar Problems
 
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+
+        for(int i=0;i<nums.length;i++){
+          if(map.containsKey(nums[i])){
+            if(i-map.get(nums[i])<=k)
+              return true;
+          }
+          map.put(nums[i],i);
+        }
+        return false;
     }
 }
